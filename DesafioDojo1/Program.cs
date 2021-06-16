a4eef740b404c31341bf1bf338502a2abb3965b3
Mateus Mello <mt.gatti@hotmail.com>
2021-06-16 18:59:58 -0300

    3 desafios completos

---
+++ DesafioDojo1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace DesafioDojo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Desafio DojoPuzzle Jokenpo
+            // https://dojopuzzles.com/problems/jokenpo/
+
+
+            System.Console.WriteLine("Escolha pedra, papel ou tesoura para o jogador 1");
+            string jogador1 = Console.ReadLine();
+            
+            System.Console.WriteLine("Escolha pedra, papel ou tesoura para o jogador 2");
+            string jogador2 = Console.ReadLine();
+
+            
+            if(jogador1 == jogador2){
+            System.Console.WriteLine("Os jogadores empataram");
+            }
+            
+
+
+            if(jogador1 == "pedra" && jogador2 == "papel"){
+                System.Console.WriteLine("O jogador 2 ganhou");
+            }
+            if(jogador1 == "papel" && jogador2 == "pedra"){
+                System.Console.WriteLine("O jogador 1 ganhou");
+            }
+
+
+            if(jogador1 == "pedra" && jogador2 == "tesoura"){
+                System.Console.WriteLine("O jogador 1 ganhou");
+            }
+            if(jogador1 == "tesoura" && jogador2 == "pedra"){
+                System.Console.WriteLine("O jogador 2 ganhou");
+            }
+
+
+            if(jogador1 == "tesoura" && jogador2 == "papel"){
+                System.Console.WriteLine("O jogador 1 ganhou");
+            }
+            if(jogador1 == "papel" && jogador2 == "tesoura"){
+                System.Console.WriteLine("O jogador 2 ganhou");
+            }
+            else{
+                System.Console.WriteLine("Por favor, digite apenas pedra, papel ou tesoura");
+            }
+        }
+    }
+}
---
+++ DesafioDojo2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DesafioDojo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Desafio DojoPuzzle FizzBuzz
+            // https://dojopuzzles.com/problems/fizzbuzz/
+            
+            for(int a = 1; a < 101; a++){
+                if(a % 3 == 0 && a % 5 == 0){
+                    System.Console.Write(" FizzBuzz -");
+                }
+                if(a % 3 == 0){
+                    System.Console.Write(" Fizz - ");
+                }
+                if(a % 5 == 0){
+                    System.Console.Write(" Buzz - ");
+                }
+                else{
+                    System.Console.Write(" " + a + " -");
+                }
+            }
+        }
+    }
+}
